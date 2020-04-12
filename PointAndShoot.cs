08b75fe129a633f89d200aee0c9495c9be361d71
kyleweeks88 <60368775+kyleweeks88@users.noreply.github.com>
2020-04-12 09:46:08 -0700

    Add files via upload

---
+++ PointAndShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndShoot : MonoBehaviour
+{
+    public GameObject crosshairs;
+    Vector3 target;
+
+
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3
+        (-Input.mousePosition.x, -Input.mousePosition.y, transform.position.z));
+    
+        crosshairs.transform.position = target;
+    }
+}
